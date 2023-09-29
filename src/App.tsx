e6e993a80b4ee1db3663020f8232fd21bef6cf1a
haseebzahid1 <mmianhaseeb444@gmail.com>
2023-09-29 12:56:14 +0500

    expenList table added for example(description,amount,category,delete,etc...

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import FormZod from "./component/FormZod";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 // import Button from "./component/Button";
 // import Alert from "./component/Alert";
 // import ListGroup from "./component/ListGroup";
@@ -13,8 +14,16 @@ import FormZod from "./component/FormZod";
 // import Array from "./component/Array";
 function App() {
  
+  const [expenses, setExpenses]= useState([
+      {id:1, description: 'aaa' , amount:10, category: 'Utilities'},
+      {id:2, description: 'bbb' , amount:10, category: 'Utilities'},
+      {id:3, description: 'ccc' , amount:10, category: 'Utilities'},
+      {id:4, description: 'ddd' , amount:10, category: 'Utilities'},
+    ])
+
+
   return (
-    <FormZod /> 
+    <ExpenseList expenses={expenses} onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))} />
   );
 }
 
